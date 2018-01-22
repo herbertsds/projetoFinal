bbdad97b09a81f3c0f1b81886545f76d0ca3b360
mrodrigues12 <mrodrigues.12.hg@gmail.com>
2018-01-22 17:20:50 -0200

    validacao das regras/perg e remocao de espacos em branco

--- Projeto Final/WebContent/pagina/funcoes.js
+++ Projeto Final/WebContent/pagina/funcoes.js
@@ -8,11 +8,42 @@ ser montada dinamicamente */
 
 
 	var vet_regras = [];
-
+	var pergunta;
+	
+	// ############# ADICAO DE REGRAS E PERGUNTA ############################################################
 	function f_AddRegra(){
-		vet_regras.push($('#regra').val());
-		adicionadas = $('#regra').val();
-		
-		$('#regrasAdicionadas').append("<br/>" + adicionadas );
-		
+		if($('#regra').val() == ""){
+			alert("Regra inválida!");
+		}
+		else{
+			vet_regras.push($('#regra').val().replace(/\s/gi, ''));
+			adicionadas = $('#regra').val();
+			
+			$('#regrasAdicionadas').append("<br/>" + adicionadas );
+			$('#regra').val("");
+		}	
 	} 
+	
+	function f_AddPergunta(){
+		if($('#pergunta').val() == ""){
+			alert("Regra inválida!");
+		}
+		else{
+			pergunta = $('#pergunta').val().replace(/\s/gi, '');
+			$('#perguntaAdicionada').append("<br/>" + $('#pergunta').val() );
+			$('#pergunta').val("");
+		}	
+	}
+	
+	// ######################################################################################################
+	
+	// ######### ESCOLHA DO TIPO DE EXERCICIO ###############################################################
+	
+	
+	
+	
+	
+	
+	
+	
+	
