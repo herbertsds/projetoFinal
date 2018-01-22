42114db69236d6733f8862fb1120a7b29be0e634
mrodrigues12 <mrodrigues.12.hg@gmail.com>
2018-01-22 11:22:35 -0200

    Adicao do modelo de pagWeb a ser usado e remocao de testes anteriores para ver se git estava ok

---
+++ Projeto Final/WebContent/pagina/funcoes.js
@@ -0,0 +1,7 @@
+// ao clicar em + armazernar as regras e a pergunta em um array
+// ir apresentando dinamicamente as regras ja adicionadas em uma area na lateral direita 
+// ao clicar em next passar para a tela de selecao de metodo
+// ao selecionar o metodo, dar algum destaque a ele 
+/* ao clicar em next, passar para a tela de exercicio, apresentando as regras aplicaveis e uma area para a arvore
+ser montada dinamicamente */ 
+// nao permitir usar regras repetidas ou invalidas
