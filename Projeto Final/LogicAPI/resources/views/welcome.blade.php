eddf778c1f7ee37885c84c648a2a8a78052c7a6c
Herbert Salazar <herbertsds.ufrj@gmail.com>
2018-02-12 13:35:02 -0200

    Adicionado tutorial de configuração do Laravel

--- Projeto Final/LogicAPI/resources/views/welcome.blade.php
+++ Projeto Final/LogicAPI/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WebService Funcionando!
                 </div>
 
                 <div class="links">
