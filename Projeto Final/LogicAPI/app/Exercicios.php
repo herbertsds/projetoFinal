4d0222f3278af71c94b43c222e02c08e68382b6e
Herbert Salazar <herbertsds.ufrj@gmail.com>
2018-03-08 11:18:53 -0300

    Problema no arquivo de exercícios corrigido

--- Projeto Final/LogicAPI/app/Exercicios.php
+++ Projeto Final/LogicAPI/app/Exercicios.php
@@ -109,9 +109,6 @@ class Exercicios extends Model
 		$this->listaExercicios['resolucao'][] = array ("((not(P))implica(((PouQ)implicaQ)e(Qimplica(PouQ))))");
 		$this->listaExercicios['resolucao'][] = array ("(Pimplica(not(Q)))","(not(not(P)ou(not(Q)))ouR)","(Rimplica(not(AeB)ou(not(S))))","(Sou(not(CouD)))","(((not(A))ou(not(B)))implicaE)","(((not(C))e(not(D)))implicaE)","(E)");
 		$this->listaExercicios['resolucao'][] = array ("not(Aimplica(not(B)))","(CouD)","((CeA)implica(FouG))","(FimplicaH)","(((not(H))ou(not(I)))implica(not(G)))","(HimplicaK)","(DimplicaJ)","((JeB)implicaK)","(K)");
-
-		/*************Copia os exercícios da lista de resolução**************/
-		$this->listaExercicios['tableaux'] = $this->listaExercicios['resolucao'];
     }
 
 }
